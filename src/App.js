603f1ba650228c689bd799c4c47f234f48eeaa9b
Jihoon <playingcards@naver.com>
2021-11-29 17:01:26 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import {useState} from 'react';
+
+function App() {
+  const [toDo, setToDo] = useState("");
+  const onChange = (event) => setToDo(event.target.value);
+  console.log(toDo);
+  return (
+    <div>
+      <input onChange={onChange} value={toDo} type="text" placeholder="Write to-do"/>
+    </div>
+  );
+}
+
+export default App;
