f45859e091fabe77b4ac2f82792332fed56d8bb3
Jihoon <playingcards@naver.com>
2021-12-05 01:29:25 -0500

    edit SCSS

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,19 +1,20 @@
 import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom';
+import '../Style/Style.scss';
 
 function Movie({id, mediumCoverImage, title, year, summary, genres}){
     return (
-        <>
+        <div className="main-container">
           <img src={mediumCoverImage} alt={title}/>
-          <h2><Link to={`/movie/${id}`}>{title}</Link></h2>
-          <p>{year}</p>
-          <p>{summary}</p>
+          <h2><Link to={`/movie/${id}`} className="image">{title}</Link></h2>
+          <p className="year">{year}</p>
+          <p className="summary">{summary}</p>
           <ul>
             {genres.map(g => (
             <li key={g}>{g}</li>
             ))}
           </ul>          
-        </>
+        </div>
     );
 }
 
